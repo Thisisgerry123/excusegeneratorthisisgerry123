adaacbee60e7cc94503bb88d0d87da2913ebb9c9
Thisisgerry123 <167911356+Thisisgerry123@users.noreply.github.com>
2024-06-04 01:58:48 +0000

    Excuse Generator done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,71 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.body.style.backgroundColor = "#678";
+
+  let who = [
+    "The dog",
+    "My grandma",
+    "Tony Hinchcliffe",
+    "Brett Kreischer",
+    "The mailman",
+    "My bird",
+    "Avatar Aang",
+    "DeezNuts",
+    "Ernesto the chicken"
+  ];
+  const personName = chooseRandomItem(who);
+
+  let action = [
+    "ate",
+    "peed on",
+    "lost",
+    "stole",
+    "crushed",
+    "ran over",
+    "sent an air strike on",
+    "broke",
+    "ripped up",
+    "sliced",
+    "demolished",
+    "nuked"
+  ];
+  const whatDid = chooseRandomItem(action);
+
+  let what = [
+    "my homework",
+    "my PlayStation 5",
+    "my phone",
+    "my car keys",
+    "the car",
+    "my textbook",
+    "on Israel",
+    "my house",
+    "my backyard",
+    "my computer"
+  ];
+  const objectBroke = chooseRandomItem(what);
+
+  let when = [
+    "before the class",
+    "after church",
+    "before church",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+    "during my class",
+    "after recess"
+  ];
+  const timeBroke = chooseRandomItem(when);
+
+  const newExcuse = `${personName} ${whatDid} ${objectBroke} ${timeBroke};`;
+
+  const p = document.querySelector("p");
+  p.textContent = newExcuse;
 };
+
+function chooseRandomItem(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
